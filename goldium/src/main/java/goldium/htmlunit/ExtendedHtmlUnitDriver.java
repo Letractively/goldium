8b99468c4c79bae8cec41830a8a714fbb7b3e58f
cgoldsby <cgoldsby@gmail.com>
2012-05-27 23:07:47 -0400

    Inital

--- goldium/src/main/java/goldium/htmlunit/ExtendedHtmlUnitDriver.java
+++ goldium/src/main/java/goldium/htmlunit/ExtendedHtmlUnitDriver.java
@@ -2,6 +2,6 @@ package goldium.htmlunit;
 
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-public class ExtendedHtmlUnitDriver extends HtmlUnitDriver {
+public class ExtendedHtmlUnitDriver extends HtmlUnitDriver implements ExtendedWebDriver {
 
 }
