1111982f661dc46eee9e95fb4de126ec25bfdd5b
cgoldsby <cgoldsby@gmail.com>
2012-05-27 23:28:17 -0400

    Adding Map<String, String> getResourceHeaders(String) to ExtendedWebDriver interface

--- goldium/src/main/java/goldium/htmlunit/ExtendedHtmlUnitDriver.java
+++ goldium/src/main/java/goldium/htmlunit/ExtendedHtmlUnitDriver.java
@@ -1,7 +1,14 @@
 package goldium.htmlunit;
 
+import java.util.Map;
+
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class ExtendedHtmlUnitDriver extends HtmlUnitDriver implements ExtendedWebDriver {
+
+	public Map<String, String> getResponseHeaders(final String url) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- goldium/src/test/java/goldium/htmlunit/ExtendedHtmlUnitDriverTest.java
+++ goldium/src/test/java/goldium/htmlunit/ExtendedHtmlUnitDriverTest.java
@@ -1,7 +1,14 @@
 package goldium.htmlunit;
 
+import org.junit.Before;
+
 public class ExtendedHtmlUnitDriverTest {
 
-	private ExtendedHtmlUnitDriver driver;
+	private ExtendedWebDriver driver;
+
+	@Before
+	public void init() {
+		driver = new ExtendedHtmlUnitDriver();
+	}
 
 }
